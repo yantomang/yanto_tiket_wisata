037a3bc918ad9604e5f8217e385426fb48699fe9
yantomang <yantomang@gmail.com>
2023-02-23 09:27:16 +0700

    menambahkan data

---
+++ user/profil.php
@@ -0,0 +1,23 @@
+<?php
+
+include "bot.php";
+
+?>
+
+<body style="background-color:#474E68;">
+    
+</body>
+
+<div class="profil">
+    <div class="container text-white">
+        <div class="icon" style=" margin-top: 10px;">
+        <i style="" class="bi bi-arrow-left"></i>
+        </div>
+        <div class="text-center">
+        <img width="200 rem" height="200 rem"style="border-radius: 100px; margin-top: 25px;" src="../img/bg5.jpg" alt="">
+        <h5>username</h5>
+        <h5>name@gamil.com</h5>
+        </div>
+        <h3 class="mt-5">pesanan saya</h3>
+    </div>
+</div>
